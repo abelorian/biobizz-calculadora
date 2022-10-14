9b1bbca119a64eb1f5bc91a1026e36728bdfc8bd
Abel O'Rian <abel.orian@gmail.com>
2022-10-14 19:07:07 -0300

    Add: basic form

---
+++ src/Calculator.js
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+
+function Calculator() {
+
+  const [cc, setCc] = useState(2000);
+  const [week, setWeek] = useState(2);
+
+	return (
+		<form>
+      <label>CC</label>
+			<input type="text" onChange={ (e) => {setCc(e.target.value)} } defaultValue={cc} />
+
+      <label>Week</label>
+      <input type="text" onChange={(e) => {setWeek(e.target.value)}} defaultValue={week} />
+
+      <label>{cc * week}</label>
+		</form>
+	);
+
+}
+
+export default Calculator;
