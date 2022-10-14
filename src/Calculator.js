f9c995db2f4f228d892eda7a0622c29fadb71c74
Abel O'Rian <abel.orian@gmail.com>
2022-10-14 19:18:35 -0300

    Fix: eslint

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,22 +1,19 @@
 import { useState } from 'react';
 
 function Calculator() {
-
   const [cc, setCc] = useState(2000);
   const [week, setWeek] = useState(2);
-
-	return (
-		<form>
+  return (
+    <form>
       <label>CC</label>
-			<input type="text" onChange={ (e) => {setCc(e.target.value)} } defaultValue={cc} />
+      <input type="text" onChange={ (e) => { setCc(e.target.value); } } defaultValue={cc} />
 
       <label>Week</label>
-      <input type="text" onChange={(e) => {setWeek(e.target.value)}} defaultValue={week} />
+      <input type="text" onChange={ (e) => { setWeek(e.target.value); } } defaultValue={week} />
 
       <label>{cc * week}</label>
-		</form>
-	);
-
+    </form>
+  );
 }
 
 export default Calculator;
