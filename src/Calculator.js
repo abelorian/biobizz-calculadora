5c5b0de79524034e5344086a72a4321a61773af0
Abel O'Rian <abel.orian@gmail.com>
2022-10-14 21:51:27 -0300

    Add: moments

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,19 +1,24 @@
 import { useState } from 'react';
+import moment from 'moment';
 
 function Calculator() {
-  const [initDate, setInitDate] = useState('2022-09-04Z-04:00');
+  const [initDate, setInitDate] = useState('04-09-2022');
   const [cc, setCc] = useState(2000);
-  const [week, setWeek] = useState(2);
+  const [totalWeeks, setTotalWeeks] = useState(12);
   const bioMix = [0, 2, 2, 2, 3, 3, 4, 4, 4, 4, 0, 0];
   const bioBloom = [0, 0, 1, 2, 2, 3, 3, 4, 4, 4, 0, 0];
   const topMax = [0, 0, 1, 1, 1, 1, 1, 4, 4, 4, 0, 0];
-  const fem = 12;
-  const auto = 9;
+
+  const style = {
+    fontWeight: 'bold',
+  }
 
   function addWeek(i) {
-    let thisWeek = new Date(initDate);
-    const date = thisWeek.setDate(thisWeek.getDate() + (i * 7));
-    return new Date(date).toLocaleString('es-CL');
+    return moment(initDate, 'DD-MM-YYYY').add(i, 'week');
+  }
+
+  function currentWeek(i) {
+    return moment().isBetween(addWeek(i), addWeek(i).endOf('week'));
   }
 
   return (
@@ -22,8 +27,11 @@ function Calculator() {
         <label>CC</label>
         <input type="text" onChange={ (e) => { setCc(e.target.value); } } defaultValue={cc} />
 
-        <label>Week</label>
+        <label>initDate</label>
         <input type="text" onChange={ (e) => { setInitDate(e.target.value); } } defaultValue={initDate} />
+
+        <label>totalWeeks</label>
+        <input type="number" onChange={ (e) => { setTotalWeeks(Math.min(12, parseInt(e.target.value))); } } defaultValue={totalWeeks} />
       </form>
       <div>
         <table className="table">
@@ -36,9 +44,9 @@ function Calculator() {
             </tr>
           </thead>
           <tbody>
-            { [...Array(auto)].map((value, i) => (
+            { [...Array(totalWeeks)].map((value, i) => (
               <tr key={i}>
-                <td>Week {i + 1} ({addWeek(i)})</td>
+                <td style={ currentWeek(i) ? style : {} }>Week {i + 1} ({ addWeek(i).format('DD-MM-YYYY') })</td>
                 <td>{(bioMix[i] * cc) / 1000} cc.</td>
                 <td>{(bioBloom[i] * cc) / 1000} cc.</td>
                 <td>{(topMax[i] * cc) / 1000} cc.</td>
